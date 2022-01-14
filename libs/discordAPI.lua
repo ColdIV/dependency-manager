b76a7fd69574876056e05e5b2a5ca89514bf4dd8
Josh <dev@cldv.org>
2022-01-14 22:48:19 +0100

    add discordAPI + example

---
+++ libs/discordAPI.lua
@@ -0,0 +1,23 @@
+local obj = {}
+
+obj.API_TOKEN = ''
+obj.API_URL = ''
+
+function obj:init(API_TOKEN, API_URL)
+    if API_TOKEN == nil or API_URL == nil then
+        return false
+    end
+
+    self.API_TOKEN = API_TOKEN
+    self.API_URL = API_URL
+    return true
+end
+
+function obj:send(message)
+    if not http.checkURL(self.API_URL) then return false end
+    http.post(self.API_URL, "token=" .. self.API_TOKEN .. "&message=" .. message)
+    --@TODO: Check response for success message; return true / false accordingly.
+    return true
+end
+
+return obj
